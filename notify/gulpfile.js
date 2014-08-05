c8cb50fcae3168e41f2f6d5818805af4c9401b20
MariMax <mma29121983@gmail.com>
2014-08-06 00:30:35 +0600

    livereload+start header

--- notify/gulpfile.js
+++ notify/gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
 var connect = require('gulp-connect');
+var nib = require('nib');
 
 gulp.task('connect', function() {
   connect.server({
-    root: '',
+    port: 8080,
     livereload: true
   });
 });
@@ -12,27 +13,22 @@ gulp.task('connect', function() {
 
 gulp.task('watch', function () {
   gulp.watch(['./css/**/*.styl'], ['stylus']);
+  gulp.watch(['./css/**/*.css','./index.html'], ['reload']);
 });
 
 gulp.task('stylus', function () {
   gulp.src('./css/**/*.styl')
-    .pipe(stylus())
-    .pipe(gulp.dest('./css'))
-    .pipe(connect.reload());
-});
-
-
-// Use nib
-// Require nib
-var nib = require('nib');
-// either a String or an Array
-gulp.task('nib', function () {
-  gulp.src('./css/nib.styl')
     .pipe(stylus({use: [nib()]}))
-    .pipe(gulp.dest('./css/nib'));
+    .pipe(gulp.dest('./css'));
 });
+
+gulp.task('reload', function () {
+	gulp.src(['./css/**/*.css','./index.html'])
+  		.pipe(connect.reload());
+});
+
 
 // Default gulp task to run
 gulp.task('default', function(){
-	gulp.run('nib','stylus','watch');
+	gulp.run('connect','stylus','watch');
 });
